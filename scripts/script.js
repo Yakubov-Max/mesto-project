d95d8d52524f14d1e895bcbc0a04068a79974076
Max <55656370+Yakubov-Max@users.noreply.github.com>
2021-08-26 13:35:54 +0300

    actual version

--- scripts/script.js
+++ scripts/script.js
@@ -20,7 +20,32 @@ const aboutInput = popupEdit.querySelector(".popup__profile-about");
 const editForm = popupEdit.querySelector(".popup__form");
 const imagePopup = document.querySelector(".popup_image");
 
-import {initialCards} from './initialCards.js';
+const initialCards = [
+  {
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+];
 
 // profile popup open/close
 
@@ -92,7 +117,7 @@ const cardLink = addForm.querySelector(".popup__add-link");
 
 function submitCard(evt) {
   evt.preventDefault();
-  cardData = {
+  let cardData = {
     cardName: cardName.value,
     cardLink: cardLink.value,
   };
